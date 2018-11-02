2af27691fa69c283f5a7ae8b33f94ebb6101fea8
omer <iomerkantar@gmail.com>
2018-11-02 04:23:58 +0300

    develop: search component gelistirildi. 🏹

--- VNGRS/Views/Components/Search/SearchComponent.swift
+++ VNGRS/Views/Components/Search/SearchComponent.swift
@@ -12,39 +12,20 @@ import RxSwift
 class SearchComponent: UIView, QueerViewLoadable {
 
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var cancelButton: UIButton!
     
-
-
     // Initialize
     override func awakeFromNib() {
         super.awakeFromNib()
         textField.font = UIFont.Style.barTitle.font
         textField.textColor = .black
-        textField.delegate = self
-        textField.attributedPlaceholder = NSAttributedString(string: "Search repository",
-                                                               attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        textField.placeholder = "Search 🚀"
+        textField.returnKeyType = .search
+//        textField.attributedPlaceholder = NSAttributedString(string: "Search repository",
+//                                                               attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
 
     }
 
-    
-    
-
-    // Interface builder action funcs
-    @IBAction func clearButtonTapped() {
-        textField.resignFirstResponder()
-    }
     
 }
 
-// MARK: - UITextFieldDelegate
-extension SearchComponent: UITextFieldDelegate {
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        cancelButton.isHidden = false
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        cancelButton.isHidden = false
-    }
-}
+
