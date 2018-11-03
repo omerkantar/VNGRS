6a25157afe80c3b9621ac84a8c828dba7782c863
omer <iomerkantar@gmail.com>
2018-11-04 01:23:39 +0300

    add: collection table view cell eklendi. Generic olacak * (title, collection view, more button)🔨

---
+++ VNGRS/Views/Cells/Table/Collection/CollectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionTableViewCell.swift
+//  VNGRS
+//
+//  Created by omer kantar on 4.11.2018.
+//  Copyright © 2018 vngrs. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
