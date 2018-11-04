489fb8d9ca0fd9251f25d6e59388fdf0c05bf396
omer <iomerkantar@gmail.com>
2018-11-04 20:16:46 +0300

    test: class, struct codable parse. 🔨

--- VNGRS/Classes/Managers/ParseManager.swift
+++ VNGRS/Classes/Managers/ParseManager.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ParseManager {
     
-    func parse<T: Codable>(_ type: [T].Type, jsonList: [[String: Any]]) -> [T] {
+    static func parse<T: Codable>(_ type: [T].Type, jsonList: [[String: Any]]) -> [T] {
         
         do {
             
@@ -29,14 +29,14 @@ class ParseManager {
     
     
     
-    func parse<T: Codable>(_ type: T.Type, json: Any) -> T? {
+    static func parse<T: Codable>(_ type: T.Type, json: [String: Any]) -> T? {
         
         do {
             let jsonAsData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
             let decode = JSONDecoder()
             let object = try decode.decode(type, from: jsonAsData)
+           
             return object
-            
         } catch let error {
             debugPrint("error \(#function) catch \(error)")
         }
--- VNGRSTests/VNGRSTests.swift
+++ VNGRSTests/VNGRSTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 @testable import VNGRS
 
+
 class VNGRSTests: XCTestCase {
 
     override func setUp() {
@@ -19,16 +20,152 @@ class VNGRSTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() {
+    func testPerformanceForObjects() {
         // This is an example of a performance test case.
+        
+        
         self.measure {
-            // Put the code you want to measure the time of here.
+            // Put the code you want to measure the time of here.            
         }
     }
+    
+    func testParser() {
+        
+        // json
+        let json = Mock.jakeWharton.json
+        
+        // class
+        let user = parse(User.self, json: json)
+        XCTAssertNil(user)
+        
+        // struct
+        let entity = parse(UserEntity.self, json: json)
+        XCTAssertNil(entity)
+    }
+    
+    
+    func parse<T: Codable>(_ type: T.Type, json: [String: Any]) -> T? {
+        
+        do {
+            let jsonAsData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            let decode = JSONDecoder()
+            let object = try decode.decode(type, from: jsonAsData)
+            
+            return object
+        } catch let error {
+            debugPrint("error \(#function) catch \(error)")
+        }
+        
+        return nil
+    }
 
+}
+
+
+
+
+public struct UserEntity: Codable {
+    
+    var id: Int
+    var login: String?
+    var name: String?
+    var avatarUrl: String?
+    
+    var numberOfPublicRepos: Int = 0
+    var numberOfGists: Int = 0
+    var numberOfFollowers: Int = 0
+    var numberOfFollowings: Int = 0
+    
+    var blog: String?
+    var bio: String?
+    var location: String?
+    var createdAt: String?
+    var company: String?
+    
+    var followersUrl: String?
+    var followingUrl: String?
+    var gitstUrl: String?
+    var reposUrl: String?
+    var websiteUrl: String?
+    
+    //Custom Keys
+    enum CodingKeys: String, CodingKey {
+        case id
+        case login
+        case name
+        case avatarUrl = "avatar_url"
+        
+        case numberOfPublicRepos = "public_repos"
+        case numberOfGists = "public_gists"
+        case numberOfFollowers = "followers"
+        case numberOfFollowings = "following"
+        
+        case blog
+        case bio
+        case location
+        case createdAt = "created_at"
+        case company
+        
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gitstUrl = "gists_url"
+        case reposUrl = "repos_url"
+        case websiteUrl = "html_url"
+        
+    }
+    
+}
+
+
+
+public class User: Codable {
+    
+    var id: Int
+    var login: String?
+    var name: String?
+    var avatarUrl: String?
+    
+    var numberOfPublicRepos: Int = 0
+    var numberOfGists: Int = 0
+    var numberOfFollowers: Int = 0
+    var numberOfFollowings: Int = 0
+    
+    var blog: String?
+    var bio: String?
+    var location: String?
+    var createdAt: String?
+    var company: String?
+    
+    var followersUrl: String?
+    var followingUrl: String?
+    var gitstUrl: String?
+    var reposUrl: String?
+    var websiteUrl: String?
+    
+    //Custom Keys
+    enum CodingKeys: String, CodingKey {
+        case id
+        case login
+        case name
+        case avatarUrl = "avatar_url"
+        
+        case numberOfPublicRepos = "public_repos"
+        case numberOfGists = "public_gists"
+        case numberOfFollowers = "followers"
+        case numberOfFollowings = "following"
+        
+        case blog
+        case bio
+        case location
+        case createdAt = "created_at"
+        case company
+        
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gitstUrl = "gists_url"
+        case reposUrl = "repos_url"
+        case websiteUrl = "html_url"
+        
+    }
 }
