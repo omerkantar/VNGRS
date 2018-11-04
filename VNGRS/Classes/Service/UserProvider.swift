5e3fbaa024e26d36aede960f6bf2af9f468b9787
omer <iomerkantar@gmail.com>
2018-11-04 12:05:36 +0300

    develop: user provider olusturuldu. 🦄

---
+++ VNGRS/Classes/Service/UserProvider.swift
@@ -0,0 +1,13 @@
+//
+//  UserProvider.swift
+//  VNGRS
+//
+//  Created by omer kantar on 4.11.2018.
+//  Copyright © 2018 vngrs. All rights reserved.
+//
+
+import UIKit
+
+class UserProvider: NSObject {
+
+}
